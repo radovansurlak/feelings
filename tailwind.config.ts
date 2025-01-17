539a91b065a5b6451ae1d4ec2a776792f812b383
Radovan Šurlák <radovansurlak@gmail.com>
2025-01-17 21:26:50 +0100

    Forces light mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,6 @@ export default {
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
-	theme: {
-		extend: {
-			colors: {
-				background: "var(--background)",
-				foreground: "var(--foreground)",
-			},
-		},
-	},
+  lightMode: 'selector',
 	plugins: [daisyui],
 } satisfies Config;
